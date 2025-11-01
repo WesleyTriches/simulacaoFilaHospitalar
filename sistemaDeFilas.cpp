be87bd8b93b265c322ecd4e6b51d5dc0b7a98701
Wesley Triches <wesleygtriches@gmail.com>
2025-11-01 11:03:44 -0300

    criacao de funcoes  e alteracoes na logica

--- sistemaDeFilas.cpp
+++ sistemaDeFilas.cpp
@@ -1,107 +1,149 @@
 #include <iostream>
 #include <queue>
 
-
 using namespace std;
 
-struct cliente{
+struct paciente{
     string nome;
     char prioridade;
     int horas, minutos;
 };
 
+struct filas{
+    queue<paciente> vermelha, amarela, verde, branca;
+};
+
+void mostrarMenu()
+{
+    cout << "=== Escolha uma opcao ===" << endl;
+    cout << "C - Cadastrar paciente" << endl;
+    cout << "A - Atender paciente" << endl;
+    cout << "D - Exibir dados das filas" << endl;
+    cout << "Q - Sair do sistema" << endl;
+}
+
 int calculoEspera(int chegadaHora, int chegadaMinutos, int horaAtendimento, int minutoAtendimento){
-    int converterMinutos = ((horaAtendimento*60 + minutoAtendimento) - (chegadaHora*60 + chegadaMinutos));
+    int converterMinutos = ((horaAtendimento * 60 + minutoAtendimento) - (chegadaHora * 60 + chegadaMinutos));
     return converterMinutos;
 }
 
-int main(){
-    queue<cliente>vermelha, amarela, verde, branca;
-    cliente aux;
-    char opcao;
-    int hh = 0, mm = 0, lotacao = 0, esperaMaxima = 0;
-    int totalAtendidos = 0, atendidosV = 0, atendidosA = 0, atendidosD = 0, atendidosB = 0;
-    do{
+void cadastro(filas &fila, int &picoLotacao){
+    paciente aux;
+    cin.ignore();
+    cout << "Digite o nome do paciente: " << endl;
+    getline(cin, aux.nome);
+    cout << "Digite a prioridade (V, A, D, B): " << endl 
+        << "Digite as horas e os minutos (HH MM): " << endl;
+    cin >> aux.prioridade >> aux.horas >> aux.minutos;
+    aux.prioridade = toupper(aux.prioridade);
+    switch (aux.prioridade){
+        case 'V':
+        {
+            fila.vermelha.push(aux);
+        }break; 
 
-        cout << "Fazer um menu" << endl; // fazer o menu com as opcoes 
-        cin >> opcao;
-        opcao = toupper(opcao);
-        switch (opcao){
-            case 'C':
-                {
-                    cin.ignore();
-                    getline(cin, aux.nome);
-                    cin >> aux.prioridade >> aux.horas >> aux.minutos;
-                    if (aux.prioridade == 'V'){
-                        vermelha.push(aux);
-                    }
-                    else if (aux.prioridade == 'A'){
-                        amarela.push(aux);
-                    }
-                    else if(aux.prioridade == 'D'){
-                        verde.push(aux);
-                    }
-                    else if(aux.prioridade == 'B'){
-                        branca.push(aux);
-                    }
-                }
-                break;
+        case 'A':
+        {
+            fila.amarela.push(aux);
+        }break;
 
-            case 'A':
-                {
-                    cin >> hh >> mm;
-                    if (!vermelha.empty()){
-                        aux = vermelha.front(); vermelha.pop(); atendidosV++; totalAtendidos ++;
-                    }
-                    else if(!amarela.empty()){
-                        aux = amarela.front(); amarela.pop(); atendidosA++; totalAtendidos ++;
+        case 'D':
+        {
+            fila.verde.push(aux);
+        }break;
 
-                    }
-                    else if(!verde.empty()){
-                        aux = verde.front(); verde.pop(); atendidosD++; totalAtendidos ++;
+        case 'B':
+        {
+            fila.branca.push(aux);
+        }break;
+    }
+    int pacientesFilas = fila.vermelha.size() + fila.amarela.size() + fila.verde.size() + fila.branca.size();
+    if(picoLotacao < pacientesFilas)
+        picoLotacao = pacientesFilas;
+}
 
-                    }
-                    else if(!branca.empty()){
-                        aux = branca.front(); branca.pop(); atendidosB++; totalAtendidos ++;
-                    }
-                    else{
-                        cout << hh << ":" << mm << " Sem pacientes aguardando atendimento" << endl;
-                        break;
-                    }
+void atender(filas &fila, int &hh, int &mm, int &esperaMaxima, int &totalAtendidos, 
+    int &atendidosV, int &atendidosA, int &atendidosD, int &atendidosB){
+        paciente pacientes;
+        if(fila.vermelha.empty() && fila.amarela.empty() && fila.verde.empty() && fila.branca.empty())
+            cout << hh << " " << mm << " Sem pacientes aguardando atendimento" << endl;
+        else if (!fila.vermelha.empty()){
+                pacientes = fila.vermelha.front();
+                fila.vermelha.pop();
+                atendidosV++;
+                totalAtendidos++;
 
-                    int espera = calculoEspera(aux.horas, aux.minutos, hh, mm);
-                    if(esperaMaxima < espera) esperaMaxima = espera;
-                    cout << "Tempo de espera" << espera << " minutos" << endl;
-                }
-                break;
+        }else if(!fila.amarela.empty()){
+                pacientes = fila.amarela.front();
+                fila.amarela.pop();
+                atendidosA++;
+                totalAtendidos++;
 
-            case 'D': 
-                {
+        }else if(!fila.verde.empty()){
+                pacientes = fila.verde.front();
+                fila.verde.pop();
+                atendidosD++;
+                totalAtendidos++;
 
-                }
-                break;
-
-            case 'Q': 
-                {
-        
-                }
-            break;
-
-            case 'S': 
-                {
-                    cout << "Saindo do sistema..." << endl;
-                }
-                break;
-            default:
-                cout << "Opcao invalida. Tente novamente." << endl;  
-
+        }else if(!fila.branca.empty()){
+                pacientes = fila.branca.front();
+                fila.branca.pop();
+                atendidosB++;
+                totalAtendidos++;
         }
 
-    } while (opcao != 'S'); // fazer a opcao pra sair
+            int espera = calculoEspera(pacientes.horas, pacientes.minutos, hh, mm);
+            if (esperaMaxima < espera)
+                esperaMaxima = espera;
+            cout << "Tempo de espera: " << espera << " minutos" << endl;
+}
 
+void relatorio(){
 
+}
 
+int main()
+{
+    filas fila;
+    char opcao;
+    int hh = 0, mm = 0, esperaMaxima = 0, picoLotacao = 0;
+    int totalAtendidos = 0, atendidosV = 0, atendidosA = 0, atendidosD = 0, atendidosB = 0;
+    do
+    {
+        mostrarMenu();
+        cin >> opcao;
+        opcao = toupper(opcao);
+        switch (opcao)
+        {
+            case 'C':
+            {
+                cadastro(fila, picoLotacao);
+            }
+            break;
 
+            case 'A':
+            {
+                cout << "Digite a hora do atendimento(HH MM): " << endl;
+                cin >> hh >> mm;
+                atender(fila, hh, mm, esperaMaxima, totalAtendidos, atendidosV, atendidosA, atendidosD, atendidosB);
+            }
+            break;
+
+            case 'D':
+            {
+            }
+            break;
+
+            case 'Q':
+            {
+            }
+            break;
+
+            default:
+                cout << "Opcao invalida. Tente novamente." << endl;
+        }
+
+    } while (opcao != 'Q'); 
 
     return 0;
 }
